64b630ab349ac9dd9d5cfe92ddc3380c69700f1f
ogadra <61941819+ogadra@users.noreply.github.com>
2021-05-23 01:36:59 +0900

    pref checkbox implemented

--- src/components/select.tsx
+++ src/components/select.tsx
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { ServerSideProps } from 'src/pages';
 
-const Select: React.FC = () => {
+const Select: React.FC = (props: ServerSideProps) => {
+    const [wrapColumns, setWrapColumns] = useState({gridTemplateColumns: `repeat(5, 1fr)`});
+    const wrap = {display: 'grid'}
+
+    useEffect(() => {
+        function handleResize() {
+            //console.log(Math.round(5.5))
+            //setWidth(window.innerWidth / 100));
+            //console.log(window.innerWidth, width);
+            setWrapColumns({gridTemplateColumns: "repeat(" + String(Math.round(window.innerWidth / 150)) + ", 1fr)"});            
+        }
+        window.addEventListener('resize', handleResize);
+        handleResize();
+        return () => window.removeEventListener('resize', handleResize);
+    },[]);
+
     const hoge = async () => {
         const response = await fetch('./api/resas', {
             method: 'POST',
@@ -12,11 +28,23 @@ const Select: React.FC = () => {
         const data = await response.json().then(data => data);
         console.log(data);
     }
+    const handleChange = (e) => {
+        console.log(e.target.id);
+    }
 
     return(
-        <ul>
-            <li onClick={hoge}>hogefuga</li>
-        </ul>
+        <div style={Object.assign(wrap, wrapColumns)}>
+            {props.prefs.map(
+                (pref) => {
+                    return(
+                        <label style={{margin:"0"}} key={pref.prefName}>
+                            <input type='checkbox' onChange={handleChange} id={pref.prefCode}/>
+                            {pref.prefName}
+                        </label>
+                    );
+                }
+            )}
+        </div>
     )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,36 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Select from '../components/select'
+import { GetServerSideProps } from 'next'
 
-const Home: React.FC = () => {
-    return (
-        <div className={styles.container}>
-          <Select/>
-        </div>
-    )
+interface Prefs{
+  prefCode: number;
+  prefName: string;
+}
+
+export interface ServerSideProps{
+  message: null;
+  result: Prefs[];
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  
+  const response = await fetch(`https://opendata.resas-portal.go.jp/api/v1/prefectures`, {
+    headers: {'X-API-KEY': process.env.RESAS_API}
+  });
+  const jsonData = await response.json();
+  return {
+    props: jsonData,
+  };
+}
+
+const Home: React.FC = (result: ServerSideProps) => {
+
+  return (
+    <div className={styles.container}>
+      <Select prefs={result.result}/>
+    </div>
+  )
 }
 
 export default Home
